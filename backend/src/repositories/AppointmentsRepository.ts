a1d891b9e38e410d8350ffed6e0c7258c38f8a12
cassiocsantana <cassiocsantana@hotmail.com>
2020-04-24 12:22:38 -0400

    Parte inicial do projeto conceitos SOLID

---
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,36 @@
+import Appointment from '../models/Appointment';
+import { isEqual } from 'date-fns';
+
+//Data Transferer Object
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor(){
+    this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointments = this.appointments.find(appointment => isEqual(date, appointment.date));
+    return findAppointments || null;
+  }
+
+  public create({provider, date}: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({provider, date});
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
