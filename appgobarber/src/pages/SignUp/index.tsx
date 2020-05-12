c056a04513519a59cede856df73939ef13c255f4
cassiocsantana <cassiocsantana@hotmail.com>
2020-05-12 18:30:09 -0400

    Cadastro de novo usuário

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -17,8 +17,10 @@ import * as Yup from 'yup';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import getValidationErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.png';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
 
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
@@ -50,12 +52,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
       Alert.alert(
         'Cadastro realizado!',
         'Você ja pode fazer seu logon no GoBarber!',
       );
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
