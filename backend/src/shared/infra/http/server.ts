a7c277988ae1087cfd29704fa03c97dfd001c3ce
cassiocsantana <cassiocsantana@hotmail.com>
2020-05-20 17:25:37 -0400

    feature inicial email in development,  forgot and reset

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -13,7 +13,7 @@ import '@shared/container';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
@@ -24,6 +24,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
+  // console.log(response);
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error',
