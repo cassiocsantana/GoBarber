148b15754883bf1ca56d3bddf122cd89642fcf22
cassiocsantana <cassiocsantana@hotmail.com>
2020-05-25 14:42:32 -0400

    Template Engine final

--- backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,11 +1,8 @@
-import IParseMailTemplateProviderDTO from '../dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
 
 class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({
-    template,
-  }: IParseMailTemplateProviderDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
 export default FakeMailTemplateProvider;
