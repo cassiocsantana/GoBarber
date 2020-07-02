d3d42c6ece07a3d1a92a6c22db620806808c0478
cassiocsantana <cassiocsantana@hotmail.com>
2020-07-01 21:04:25 -0400

    Listagem para provedores

--- backend/src/modules/appointements/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointements/infra/http/routes/appointments.routes.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 
 import ensureAuthenticaded from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controller/AppointementsController';
+import ProviderAppointmentsController from '../controller/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 appointmentsRouter.use(ensureAuthenticaded);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
