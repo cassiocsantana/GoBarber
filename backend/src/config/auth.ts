a01c80908163f061528e094e44ada6d642220230
cassiocsantana <cassiocsantana@hotmail.com>
2020-07-20 20:47:19 -0400

    Variaveis de ambiente

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,7 @@
 export default {
   jwt: {
-    secret: '1fd77422011a2a40a378461ef22a041d',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
+// secret: '1fd77422011a2a40a378461ef22a041d',
